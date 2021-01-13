935c9ec24a98d27f657c3d660953cc5f9ef0b961
davidhin <hello@davidhin.com>
2021-01-14 10:00:55 +1030

    Add login to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Application from "./Components/Application";
+import fire from "./fire.js";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  fire.auth().onAuthStateChanged((user) => {
+    return user ? setIsLoggedIn(true) : setIsLoggedIn(false);
+  });
+
   return (
     <Router>
-      <Application />
+      <Application isLoggedIn={isLoggedIn} />
     </Router>
   );
 }
