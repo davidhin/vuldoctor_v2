3da12a70c8e7cb803ce32581ae118c0299d04638
davidhin <hello@davidhin.com>
2021-01-14 11:22:57 +1030

    Add token authentication client side

---
+++ src/Components/Authentication.js
@@ -0,0 +1,13 @@
+import fire from "../fire";
+
+export const createToken = async () => {
+  const user = fire.auth().currentUser;
+  const token = user && (await user.getIdToken());
+  const payloadHeader = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  return payloadHeader;
+};
