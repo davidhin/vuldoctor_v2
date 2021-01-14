108f2df0ec8eaa4c3c9d3d707eb8d6730e4ed673
davidhin <hello@davidhin.com>
2021-01-14 23:47:53 +1030

    Add link to react index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
+// ps://dev.to/myogeshchavan97/how-to-deploy-react-node-js-application-to-heroku-4jb4
