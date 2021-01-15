07df77364bb86ac1bf31b98b36305ffd0be1eacd
davidhin <hello@davidhin.com>
2021-01-15 16:32:20 +1030

    Fix redirection bug

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Application from "./Components/Application";
 import fire from "./fire.js";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(null);
 
   fire.auth().onAuthStateChanged((user) => {
     return user ? setIsLoggedIn(true) : setIsLoggedIn(false);
