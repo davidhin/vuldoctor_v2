f43756cb39f3ca55f65bcb35f2ba4f4ee5cfee76
davidhin <hello@davidhin.com>
2021-01-17 22:18:58 +1030

    Fix GCS Credentials

--- server/gcs.js
+++ server/gcs.js
@@ -1,6 +1,16 @@
 const { Storage } = require("@google-cloud/storage");
+const dotenv = require("dotenv");
+dotenv.config({ path: "../.env" });
+const { SOLID_MANTRA_PRIVATE_KEY } = process.env;
 
-const storage = new Storage();
+const storage = new Storage({
+  projectId: "solid-mantra-301604",
+  credentials: {
+    private_key: SOLID_MANTRA_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    client_email:
+      "firebase-adminsdk-gb291@solid-mantra-301604.iam.gserviceaccount.com",
+  },
+});
 
 const downloadStr = async (filename) => {
   let res = await storage.bucket("vuldoctor2").file(filename).download();
