312299224da966ef8a0c014f5d44ef3ac7fb8758
davidhin <hello@davidhin.com>
2021-01-19 13:48:32 +1030

    Update dashboard with processing

--- src/Components/Pages/Dashboard.js
+++ src/Components/Pages/Dashboard.js
@@ -4,6 +4,7 @@ import Paper from "@material-ui/core/Paper";
 import { makeStyles } from "@material-ui/core/styles";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import fire from "../../fire.js";
 import { createToken } from "../Authentication";
 import Demo from "../Report/Demo";
 import ProjectsTable from "../Tables/ProjectsTable";
@@ -22,7 +23,8 @@ const useStyles = makeStyles((theme) => ({
 
 function Dashboard(props) {
   const classes = useStyles();
-  const [projects, setProjects] = useState([{ projectid: "1234" }]);
+  const [projects, setProjects] = useState([]);
+  const [processing, setProcessing] = useState(null);
 
   const getProjects = async () => {
     const header = await createToken(props.user);
@@ -32,21 +34,29 @@ function Dashboard(props) {
 
   useEffect(() => {
     props.changePage("Dashboard");
-  });
+    let pstatus = fire.database().ref("/" + props.user.uid);
+    const l = pstatus.on("value", (d) => {
+      if (d) setProcessing(d.val() ? d.val() : "No tasks running");
+    });
+    return () => l();
+  }, []);
 
   return (
     <div className={classes.root}>
       <Grid container spacing={2}>
-        <Grid item xs={12} sm={12}>
+        <Grid item xs={12} sm={9}>
           <Paper className={classes.paper}>
             <FileUpload user={props.user} getProjects={getProjects} />
           </Paper>
         </Grid>
-        <Grid item xs={12} sm={12} md={12} lg={6}>
-          <Paper className={classes.paper}>This is a Bar Plot</Paper>
-          <Demo />
+        <Grid item xs={12} sm={3} md={3}>
+          <Paper className={classes.paper}>
+            Running Tasks
+            <br />
+            {JSON.stringify(processing)}
+          </Paper>
         </Grid>
-        <Grid item xs={12} sm={6}>
+        <Grid item xs={12}>
           <Paper className={classes.paper}>
             {!props.user ? (
               <CircularProgress />
@@ -59,11 +69,9 @@ function Dashboard(props) {
             )}
           </Paper>
         </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        <Grid item xs={12} sm={12} md={12} lg={6}>
+          <Paper className={classes.paper}>This is a Bar Plot</Paper>
+          <Demo />
         </Grid>
         <Grid item xs={12} sm={6} md={3}>
           <Paper className={classes.paper}>xs=6 sm=3</Paper>
