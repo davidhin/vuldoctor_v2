69d57d3b710ae1b86236bc3d9a474eea071885ca
davidhin <hello@davidhin.com>
2021-01-19 23:10:09 +1030

    Update dashboard to dynamicaly show processing

--- src/Components/Pages/Dashboard.js
+++ src/Components/Pages/Dashboard.js
@@ -1,4 +1,4 @@
-import CircularProgress from "@material-ui/core/CircularProgress";
+import { Typography } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import { makeStyles } from "@material-ui/core/styles";
@@ -18,6 +18,7 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(2),
     textAlign: "center",
     color: theme.palette.text.secondary,
+    height: "100%",
   },
 }));
 
@@ -25,18 +26,41 @@ function Dashboard(props) {
   const classes = useStyles();
   const [projects, setProjects] = useState([]);
   const [processing, setProcessing] = useState(null);
+  const [loadProjects, setLoadProjects] = useState(true);
 
   const getProjects = async () => {
+    setLoadProjects(true);
     const header = await createToken(props.user);
     let res = await axios.get("/getProjects", header);
-    setProjects(res["data"]);
+    res = res["data"];
+    res.map((x) => (x["status"] = "..."));
+    setProjects(res);
+  };
+
+  const setProjProc = async (d, header) => {
+    let res = await axios.get("/getProjects", header);
+    let a = res["data"];
+    if (d.val()) {
+      a.map(
+        (x) => (x["status"] = d.val()[x.pid] ? "Processing..." : "Complete")
+      );
+    } else {
+      a.map((x) => (x["status"] = "Complete"));
+    }
+    setProjects(a);
+    setLoadProjects(false);
+    console.log("SET");
   };
 
   useEffect(() => {
     props.changePage("Dashboard");
     let pstatus = fire.database().ref("/" + props.user.uid);
-    const l = pstatus.on("value", (d) => {
-      if (d) setProcessing(d.val() ? d.val() : "No tasks running");
+    const l = pstatus.on("value", async (d) => {
+      if (d) {
+        setProcessing(d.val() ? d.val() : null);
+        const header = await createToken(props.user);
+        setProjProc(d, header);
+      }
     });
     return () => l();
   }, []);
@@ -51,33 +75,37 @@ function Dashboard(props) {
         </Grid>
         <Grid item xs={12} sm={3} md={3}>
           <Paper className={classes.paper}>
-            Running Tasks
+            <Typography variant="h5">Running Tasks</Typography>
             <br />
-            {JSON.stringify(processing)}
+            <div>
+              {!processing
+                ? "No tasks running"
+                : Object.keys(processing).map((k) => (
+                    <Paper className={classes.paper} style={{ margin: "10px" }}>
+                      {k}
+                    </Paper>
+                  ))}
+            </div>
           </Paper>
         </Grid>
         <Grid item xs={12}>
           <Paper className={classes.paper}>
-            {!props.user ? (
-              <CircularProgress />
-            ) : (
-              <ProjectsTable
-                user={props.user}
-                projects={projects}
-                getProjects={getProjects}
-              />
-            )}
+            <ProjectsTable
+              user={props.user}
+              projects={projects}
+              getProjects={getProjects}
+              loading={loadProjects}
+            />
           </Paper>
         </Grid>
         <Grid item xs={12} sm={12} md={12} lg={6}>
-          <Paper className={classes.paper}>This is a Bar Plot</Paper>
-          <Demo />
+          <Paper className={classes.paper}>
+            <Demo />
+          </Paper>
         </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+
+        <Grid item xs={12} sm={12} md={12} lg={6}>
+          <Paper className={classes.paper}></Paper>
         </Grid>
       </Grid>
     </div>
--- src/Components/Tables/ProjectsTable.js
+++ src/Components/Tables/ProjectsTable.js
@@ -1,51 +1,40 @@
-// https://github.com/mbrn/material-table
-import CircularProgress from "@material-ui/core/CircularProgress";
 import MaterialTable from "material-table";
 import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { TABLEICONS } from "./tableIcons";
 
 const ProjectsTable = (props) => {
-  let data = props.projects;
-  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
   const history = useHistory();
 
   useEffect(() => {
-    if (props.user) {
-      const run = async () => {
-        await props.getProjects();
-        setLoading(false);
-      };
-      run();
-    }
-  }, []);
+    setData(props.projects);
+  });
 
   return (
     <div style={{ maxWidth: "100%" }}>
-      {loading ? (
-        <CircularProgress />
-      ) : (
-        <MaterialTable
-          icons={TABLEICONS}
-          columns={[
-            { title: "Project ID", field: "pid" },
-            { title: "Project Name", field: "name" },
-          ]}
-          data={data}
-          title="Projects"
-          options={{
-            pageSize: 10,
-            pageSizeOptions: [10, 20, 50],
-          }}
-          actions={[
-            {
-              icon: "v",
-              tooltip: "View Project",
-              onClick: (event, row) => history.push(`/report/${row.pid}`),
-            },
-          ]}
-        />
-      )}
+      <MaterialTable
+        icons={TABLEICONS}
+        columns={[
+          { title: "Project ID", field: "pid" },
+          { title: "Project Name", field: "name" },
+          { title: "Status", field: "status" },
+        ]}
+        data={data}
+        title="Projects"
+        options={{
+          pageSize: 10,
+          pageSizeOptions: [10, 20, 50],
+        }}
+        isLoading={props.loading}
+        actions={[
+          {
+            icon: "v",
+            tooltip: "View Project",
+            onClick: (event, row) => history.push(`/report/${row.pid}`),
+          },
+        ]}
+      />
     </div>
   );
 };
