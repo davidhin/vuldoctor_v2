fb417c3ee88ac9e810eb5fc57289af22f3c4dea5
davidhin <hello@davidhin.com>
2021-01-20 13:44:10 +1030

    Add material ui grid CVSS plot

--- src/Components/Pages/Report.js
+++ src/Components/Pages/Report.js
@@ -1,11 +1,27 @@
 import CircularProgress from "@material-ui/core/CircularProgress";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { createToken } from "../Authentication";
 import CVSSPlot from "../Report/CVSSPlot";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+    height: "100%",
+  },
+}));
+
 const Report = (props) => {
+  const classes = useStyles();
   let changePage = props.changePage;
   let user = props.user;
   let { projectid } = useParams();
@@ -28,7 +44,23 @@ const Report = (props) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return <div>{loading ? <CircularProgress /> : <CVSSPlot scan={deps} />}</div>;
+  return (
+    <div>
+      {loading ? (
+        <CircularProgress />
+      ) : (
+        <div>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Paper className={classes.paper}>
+                <CVSSPlot scan={deps} />
+              </Paper>
+            </Grid>
+          </Grid>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Report;
--- src/Components/Report/CVSSPlot.js
+++ src/Components/Report/CVSSPlot.js
@@ -1,6 +1,22 @@
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
 import React, { useEffect, useState } from "react";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+    height: "100%",
+  },
+}));
+
 const CVSSPlot = (props) => {
+  const classes = useStyles();
   const [scan, setScan] = useState(props.scan);
 
   useEffect(() => {
@@ -8,7 +24,13 @@ const CVSSPlot = (props) => {
     console.log(scan);
   }, [props.scan]);
 
-  return <div></div>;
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12} lg={9}>
+        <Paper className={classes.paper}>dsdf</Paper>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default CVSSPlot;
