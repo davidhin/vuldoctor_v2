de7a9b141d1387f6a781dcf84c5ac66a53a0b983
davidhin <hello@davidhin.com>
2021-01-25 15:36:50 +1030

    Add github route to depcheck

--- microservices/dep_check/mongoatlas.py
+++ microservices/dep_check/mongoatlas.py
@@ -1,18 +1,31 @@
-import os
-
-import pymongo
+from datetime import datetime
 
 
-def get_github_token(uid):
+def get_github_token(uid, client):
     """Connect to MongoDB and obtain GitHub personal access token from uid.
 
     Args:
         uid ([str]): user id (firebase auth)
+        client ([mongodbclient]): mongodb client
 
     Returns:
         [str]: UID's personal access token for GitHub
     """
-    # Connect to MongoDB
-    client = pymongo.MongoClient(os.getenv("MONGODB_TOKEN"))
     db = client.main.users
     return db.find_one({"uid": uid})["ghtoken"]
+
+
+def update_last_checked_date(uid, projectid, client):
+    """Connect to MongoDB and updaate last_checked_date given projectid.
+
+    Args:
+        projectid ([str]): string representation of projectid
+        client ([mongodbclient]): mongodb client
+
+    """
+    today = datetime.now()
+    d1 = today.strftime("%d/%m/%Y, %I:%M:%S %P").replace(" 0", " ")
+    client.main.projects.update_one(
+        {"uid": uid, "projects": {"$elemMatch": {"pid": projectid}}},
+        {"$set": {"projects.$.date": d1}},
+    )
--- microservices/dep_check/routes.py
+++ microservices/dep_check/routes.py
@@ -1,11 +1,15 @@
+import os
+from glob import glob
 from pathlib import Path
 
+import pymongo
 from flask import Blueprint, Response, request
 from flask_cors import CORS
 from google.cloud import storage
 
-from depscan import depscan, serialise_request_files
+from depscan import allowed_file, depscan, serialise_request_files
 from fire import authenticateToken, deleteDB, setDB
+from mongoatlas import get_github_token, update_last_checked_date
 
 # Registering routes to 'routes' so they can be accessed in other files
 routes = Blueprint("urls", __name__,)
@@ -16,6 +20,62 @@ gcs = storage.Client()
 
 # Get the bucket that the file will be uploaded to.
 bucket = gcs.get_bucket("vuldoctor2")
+
+# Connect to mongodb
+client = pymongo.MongoClient(os.getenv("MONGODB_TOKEN"))
+
+
+@routes.route("/run_github", methods=["POST"])
+def run_github():
+    """Perform depscan through web interface GitHub reload.
+
+    Returns:
+        [response]: 200 if success, 400/204 if failure
+    """
+    # Authenticate User
+    try:
+        decoded_token = authenticateToken(request)
+        uid = decoded_token["uid"]
+        request_data = request.get_json(force=True)
+        github_token = get_github_token(uid, client)
+        reponame = request_data["name"]
+        projectid = request_data["pid"]
+    except Exception as e:
+        print(e)
+        resp = Response(status=403)
+        return resp
+
+    # Set Processing
+    setDB(uid, projectid)
+
+    # Create working directory
+    filedir = "upload/" + uid + "/" + projectid + "/"
+    Path(filedir).mkdir(parents=True, exist_ok=True)
+
+    # Clone GitHub to working directory
+    os.system(
+        "git clone https://{}@github.com/{}.git {}".format(
+            github_token, reponame, filedir
+        )
+    )
+
+    # Get files
+    files = glob(filedir + "**", recursive=True)
+    files = [i for i in files if allowed_file(i)]
+
+    # Perform dep scanning
+    depscan_result = depscan(files, filedir, bucket, uid, projectid)
+
+    if not depscan_result:
+        deleteDB(uid, projectid)
+        return Response(status=400)
+
+    # Set mongodb lastcheckeddate
+    update_last_checked_date(uid, projectid, client)
+
+    # Unset processing
+    deleteDB(uid, projectid)
+    return Response(status=200)
 
 
 @routes.route("/", methods=["POST"])
