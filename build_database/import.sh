5cad8360df9b2d2aa4be446067c1f6f26d251447
davidhin <hello@davidhin.com>
2021-01-26 14:00:14 +1030

    Prepare moving build_database to microservice

--- build_database/import.sh
+++
@@ -1,3 +0,0 @@
-mongoimport --uri "mongodb+srv://davidhin:$MONGODB_PASSWORD@vulcluster.zqpux.mongodb.net/main?retryWrites=true&w=majority" --collection cve --drop --file cve.json --jsonArray
-mongoimport --uri "mongodb+srv://davidhin:$MONGODB_PASSWORD@vulcluster.zqpux.mongodb.net/main?retryWrites=true&w=majority" --collection cpe --drop --file cpe.json --jsonArray
-mongoimport --uri "mongodb+srv://davidhin:$MONGODB_PASSWORD@vulcluster.zqpux.mongodb.net/main?retryWrites=true&w=majority" --collection cvecpe --drop --file cvecpe.json --jsonArray
