b51ab178a749977d4b92fc7899b2bd1a3d87d88e
davidhin <hello@davidhin.com>
2021-01-27 10:49:27 +1030

    Remove unneeded test code

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,6 @@
 var express = require("express");
 var cveCtrl = require("./controllers/cveController");
 var cpeCtrl = require("./controllers/cpeController");
-var testCtrl = require("./controllers/testController");
 var ghCtrl = require("./controllers/ghController");
 var projectCtrl = require("./controllers/projectController");
 
@@ -10,8 +9,6 @@ var router = express.Router();
 router.route("/search/cve").get(cveCtrl.getCVEs);
 router.route("/search/cpe").get(cpeCtrl.getCPEs);
 router.route("/getCVEList").post(cveCtrl.getCVEList);
-router.route("/ping").get(testCtrl.getPing);
-router.route("/ping").post(testCtrl.postPing);
 router.route("/gh").post(ghCtrl.postGHtoken);
 router.route("/gh").get(ghCtrl.getGHtoken);
 router.route("/getreport/:projectid").get(projectCtrl.getReport);
--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,50 +1,11 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { createToken } from "../Authentication";
+import React, { useEffect } from "react";
 
 function Home(props) {
-  const [name, setName] = useState();
-
   useEffect(() => {
     props.changePage("Home");
   });
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    console.log(`submitted: ${name} `);
-    const header = await createToken(props.user);
-    try {
-      const payload = { name };
-      axios.post("/ping", payload, header);
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
-  const getEntries = async () => {
-    const header = await createToken(props.user);
-    try {
-      const res = await axios.get("/ping", header);
-      console.log(res.data);
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Name"
-          onChange={({ target }) => setName(target.value)}
-        />
-        <br />
-        <button type="submit">Submit</button>
-      </form>
-      <button onClick={getEntries}>GetEntries</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default Home;
