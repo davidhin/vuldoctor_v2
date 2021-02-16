f2b2c52806c89a56fa74a65f039a8eb80fe80517
davidhin <hello@davidhin.com>
2021-02-16 13:01:58 +1030

    Add additional info for capec-cwe

--- microservices/nvd_refresh/capec_cwe_mappings.py
+++ microservices/nvd_refresh/capec_cwe_mappings.py
@@ -16,11 +16,18 @@ attack_patterns = root[0]
 # Parse CAPECS
 capec_cwe = []
 for ap in attack_patterns:
-    data = ap.attrib
+    data = ap.attrib.copy()
+    data["severity"] = ""
+    data["likelihood"] = ""
     cwes = []
     for a in ap.iter():
         if "CWE_ID" in a.attrib:
             cwes.append(a.attrib["CWE_ID"])
+        if "Likelihood_Of_Attack" in a.tag:
+            data["likelihood"] = a.text
+        if "Typical_Severity" in a.tag:
+            data["severity"] = a.text
+    data["desc"] = ap[0].text
     data["cwes"] = cwes
     capec_cwe.append(data)
 capec_df = pd.DataFrame(capec_cwe)
@@ -29,7 +36,16 @@ capec_df = pd.DataFrame(capec_cwe)
 capec_cwe = []
 for row in capec_df.itertuples():
     for cwe in row.cwes:
-        capec_cwe.append({"capec_id": row.ID, "capec_name": row.Name, "cwe_id": cwe})
+        capec_cwe.append(
+            {
+                "capec_id": row.ID,
+                "capec_name": row.Name,
+                "cwe_id": cwe,
+                "capec_desc": row.desc,
+                "likelihood": row.likelihood,
+                "severity": row.severity,
+            }
+        )
 
 # Upload to mongoDB
 with open("capec_cwe.json", "w", encoding="utf-8") as f:
